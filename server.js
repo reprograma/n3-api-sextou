d9933eeeab45a28a64f5b06a627460620a9219de
Leticia Tiveron <leticiativeronbt@gmail.com>
2019-11-21 22:16:49 -0300

    Add index route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require('./src/app')
+const app = require('./src/app.js')
 const port = 3000
 
-app.list(port, () => {
+app.listen(port, () => {
   console.log(`App rodando na porta ${port}`)
 })
--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const index = require("./routes/index")
+
 const app = express()
+
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*")
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+
+app.use(bodyParser.json())
+
+app.use("/", index)
 
 module.exports = app
